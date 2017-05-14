d9aba559d0cbe3b1e31d285f386aca310ebe7829
Johnny Soong <johnnysoong@hotmail.com>
2017-05-14 21:44:17 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,8 +1,4 @@
 # code the # method he
 def greeting(name)
-puts "Hi! I'm HAL, what's your name?"
-gets.strip
-puts "Hello #{name}. It's nice to meet you."
+  puts "Hello #{name}. It's nice to meet you."
 end
-greeting("Don")
-
