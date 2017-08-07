8701af78a0721e2fca8d30102fc094739184af9a
Robert Aguilar <aguilar.r@live.com>
2017-08-07 19:20:16 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,16 @@
-# code the #greeting method here!
+def greeting(name)
+puts "Hello #{name}. It's nice to meet you."
+end
+
+
+#def greeting(name)
+#  puts "Hello #{name}. It's nice to meet you."
+#
+#name = gets.strip
+
+#greeting(name)
+
+
+
+
+   # code the #greeting method here!
