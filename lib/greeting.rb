9a205ddb65e1f8ec6e122b0ce26dbae90089558d
SOYEONG OH <axionoso@gmail.com>
2019-03-11 14:35:32 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,5 @@
 # code the #greeting method here!
 def greeting(name)
-  greeting = "Hello #{name}. It's nice to meet you.".strip
-  puts greeting
+  greet = "Hello #{name.strip}. It's nice to meet you."
+  puts greet
 end
