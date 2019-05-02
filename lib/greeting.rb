1c8ae115556e8b196644a7907dc64bac85dd2b1c
Samuel Jean-Rosemond <js.rosemond@gmail.com>
2019-05-02 12:13:49 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,10 @@
 # code the #greeting method here!
+
+def greeting(name)
+
+#puts "unregistering 'fleshy thing'"
+#puts "registering #{name}. Hello #{name}, 'fleshy thing', sorry."
+puts "Hello #{name.chomp.strip}. It's nice to meet you."
+#puts "meat you?"
+
+end
