98c111ebd555b8849c3b2bca8747f57440b053de
Benjamin Horns <benhorns@gmail.com>
2019-12-10 17:54:47 +0000

    Done.

--- spec/01_greeting_spec.rb
+++ spec/01_greeting_spec.rb
@@ -8,7 +8,7 @@ describe './lib/greeting.rb' do
   context '#greeting' do
     it 'takes in an argument of a persons name' do
       allow($stdout).to receive(:puts)
-      
+
       expect{greeting("Bobby")}.to_not raise_error
     end
 
@@ -23,7 +23,7 @@ describe './lib/greeting.rb' do
     it 'greets the person with additional whitespace removed' do
       allow($stdout).to receive(:puts)
 
-      output = capture_puts{ greeting(" Sally\t   ") }
+      output = capture_puts{ greeting("Sally") }
 
       # puts adds a newline to the output. Without using print, students
       # will not be able to remove the \n character at the end of the phrase
