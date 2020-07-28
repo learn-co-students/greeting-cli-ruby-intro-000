7b88a6c2170243b75ec94ac7c5ed533a37e8168a
Noah Alston <crashlive112@gmail.com>
2020-07-28 21:36:54 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,9 @@
 # code the #greeting method here!
+#Greet the user.
+#Ask the user to input their name.
+#Capture and store the user's input using the #gets method.
+#Use that captured input to puts out a string that greets
+#the user by name, using string interpolation.
+def greeting(name)
+puts "Hello #{name}. It's nice to meet you."
+end
